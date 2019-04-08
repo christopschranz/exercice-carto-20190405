aee82c1416ba531df53917030cc9a686ade999f1
Christophe Schranz <christophe.schranz@heig-vd.ch>
2019-04-08 21:39:18 +0200

    buildings v1

--- data/getBuildings.js
+++ data/getBuildings.js
@@ -3,11 +3,9 @@ const points = require('./points.json')
 const write = require('./writeCollection')
 
 const features = R.prop('features', points)
-features.filter(d => d['properties'].natural != undefined)
-
 
 write(
-  'trees',
-  features.filter(R.pathEq(['properties', 'natural'], 'tree'))
+  'buildings',
+  features.filter(R.pathEq(['properties', 'natural'], undefined))
 )
 .catch(console.log)
